ebb051a9a32244d672145300f4ea8da56c8a7b96
Francisco Lopez <franlopbri@gmail.com>
2022-03-19 11:34:58 -0600

    Creando vistas para la aplicacion

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex /poolls/5/
+    path('<int:question_id>', views.detail, name='detail'),
+    # ex /poolls/5/detail
+    path('<int:question_id>/detail', views.detail, name='detail'),
+    # ex /polls/5/results
+    path('<int:question_id>/results', views.results, name='results'),
+    # ex /polls/5/vote
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Estás en la página principal de Premios Platzi App")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta {question_id}")
