b8fce4cd2f3b4118967662eaf20e4fb49bda8078
Francisco Lopez <franlopbri@gmail.com>
2022-03-20 13:29:21 -0600

    Corrigiendo error y creando 2 test mas

--- polls/tests.py
+++ polls/tests.py
@@ -12,3 +12,15 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text="¿Quien es el mejor CD de Platzi?", pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_now_or_a_day_ago(self):
+        """was_published_recently returns True for questions created now or a day ago"""
+
+        now_question = Question(question_text="¿Quien es el mejor CD de Platzi?", pub_date=timezone.now())
+        self.assertTrue(now_question.was_published_recently())
+
+    def test_was_published_earlier_than_a_day(self):
+        """was_published_recently returns True for questions created earlier than a day"""
+
+        now_question = Question(question_text="¿Quien es el mejor CD de Platzi?", pub_date=timezone.now())
+        self.assertTrue(now_question.was_published_recently())
