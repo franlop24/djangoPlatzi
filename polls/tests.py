a713f84d457ef1a79fe845d108f08337b2924cd1
Francisco Lopez <franlopbri@gmail.com>
2022-03-20 13:18:03 -0600

    Escribiendo nuestro primer test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from polls.models import Question
 
 # Create your tests here.
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_feature_questions(self):
+        """was_published_recently returns False for questions whose pub_date in in the future"""
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="¿Quien es el mejor CD de Platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
